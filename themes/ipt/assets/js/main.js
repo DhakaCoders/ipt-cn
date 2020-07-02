a2b75ea370073012bc9b1867db957b50763579be
Rannojit <cserannojitsarkar22@gmail.com>
2020-07-03 01:40:52 +0600

    .

--- themes/ipt/assets/js/main.js
+++ themes/ipt/assets/js/main.js
@@ -275,6 +275,47 @@ if( $('.hmProBxesSlider').length ){
     });
 }
 
+
+if( $('.hmFeaBoxsSecSlider').length ){
+    $('.hmFeaBoxsSecSlider').slick({
+      dots: false,
+      infinite: false,
+      autoplay: false,
+      autoplaySpeed: 2000,
+      speed: 300,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+}
+
 if( $('.hmReferencesSlider').length ){
     $('.hmReferencesSlider').slick({
       dots: false,
