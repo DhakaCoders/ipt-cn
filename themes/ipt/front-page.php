b558e4f1be018cf48671287462ffd65b671b4ebf
azizulsoftsw <azizul.softsw@gmail.com>
2020-07-12 17:54:25 +0600

    az

--- themes/ipt/front-page.php
+++ themes/ipt/front-page.php
@@ -1,24 +1,41 @@
-<?php get_header(); ?>
+<?php 
+get_header(); 
+?>
 
 <section class="main-bnr-con-sec">
+  <?php 
+    $intro = get_field('introsec', HOMEID);
+    if( $intro ): 
+
+    $bafbeelding = '';
+    if( !empty($intro['afbeelding']) ){
+      $bafbeelding = cbv_get_image_src($intro['afbeelding']);
+    }
+  ?>
   <div class="main-bnr-con-cntlr">
     <div class="main-bnr-cntlr">
-      <div class="main-bnr-con inline-bg" style="background: url(<?php echo THEME_URI; ?>/assets/images/main-bnr-img-01.jpg);">
+      <div class="main-bnr-con inline-bg" style="background: url(<?php echo $bafbeelding; ?>);">
         <div class="main-bnr-con-des">
           <div class="container">
             <div class="row">
               <div class="col-sm-12">
                 <div class="main-bnr-con-des-inr">
-                  <h3 class="main-brn-sub-title">Industrial Textile Products</h3>
-                  <strong class="main-bnr-title">Praesent justo consectetur augue.</strong>
-                  <p>Aliquam ipsum sit magna est nulla nulla. Dictum feugiat consectetur in<br> mauris, scelerisque mattis netus gravida.</p>
+                  <?php 
+                    if( !empty( $intro['subtitel'] ) ) printf( '<h3 class="main-brn-sub-title">%s</h3>', $intro['subtitel']); 
+                    if( !empty( $intro['titel'] ) ) printf( '<strong class="main-bnr-title">%s</strong>', $intro['titel']); 
+                    if( !empty( $intro['beschrijving'] ) ) echo wpautop($intro['beschrijving']); 
+                  ?>
                   <div class="main-bnr-con-des-btns">
-                    <div class="main-bnr-des-btn main-bnr-des-btn-1">
-                      <a href="#">Over Ons</a>
-                    </div>
-                    <div class="main-bnr-des-btn main-bnr-des-btn-2">
-                      <a href="#">Onze Referenties</a>
-                    </div>
+                    <?php 
+                      $knop1 = $intro['knop_1'];
+                      $knop2 = $intro['knop_2'];
+                      if( is_array( $knop1 ) &&  !empty( $knop1['url'] ) ){
+                          printf('<div class="main-bnr-des-btn main-bnr-des-btn-1"><a href="%s" target="%s">%s</a></div>', $knop1['url'], $knop1['target'], $knop1['title']); 
+                      } 
+                      if( is_array( $knop2 ) &&  !empty( $knop2['url'] ) ){
+                          printf('<div class="main-bnr-des-btn main-bnr-des-btn-2"><a href="%s" target="%s">%s</a></div>', $knop2['url'], $knop2['target'], $knop2['title']); 
+                      } 
+                    ?>
                   </div>
                 </div>
               </div>
@@ -28,14 +45,24 @@
       </div>
     </div>
   </div>
+  <?php endif; ?>
+ <?php 
+    $showhide_troef = get_field('showhide_troef', HOMEID);
+    if( $showhide_troef ):
+      $htroefs = get_field('home_troefs', HOMEID);
+      if($htroefs):
+  ?>
   <div class="hm-fea-boxs-section">
     <div class="container">
       <div class="row">
         <div class="col-sm-12">
           <div class="hm-fea-boxs-sec-cntlr  hmFeaBoxsSecSlider clearfix">
+            <?php foreach( $htroefs as $htroef ): ?>
             <div class="hm-fea-box-item-col">
               <div class="hm-fea-box-item mHc">
-                <a href="#" class="overlay-link"></a>
+                <?php if( $htroef['knop'] ): ?>
+                <a href="<?php echo $htroef['knop']; ?>" class="overlay-link"></a>
+                <?php endif; ?>
                 <div class="hm-fea-box-item-icon mHc1">
                   <i>
                     <svg class="hm-fea-box-item-icon-svg" width="38" height="38" viewBox="0 0 38 38" fill="#fff">
@@ -43,61 +70,40 @@
                     </svg> 
                   </i>
                 </div>
-                <h4 class="hm-fea-box-item-title mHc2">Troef</h4>
-                <p>Quisque ante ex, facilisis in elit vel, pulvinar vestibulum ligula.</p>
+                <?php  
+                  if( !empty( $htroef['titel'] ) ) printf( '<h4 class="hm-fea-box-item-title mHc2">%s</h4>', $htroef['titel']); 
+                  if( !empty( $htroef['beschrijving'] ) ) echo wpautop($htroef['beschrijving']); 
+                ?>
               </div>
             </div>
-            <div class="hm-fea-box-item-col">
-              <div class="hm-fea-box-item mHc">
-                <a href="#" class="overlay-link"></a>
-                <div class="hm-fea-box-item-icon mHc1">
-                  <i>
-                    <svg class="hm-fea-box-item-icon-svg" width="38" height="38" viewBox="0 0 38 38" fill="#fff">
-                      <use xlink:href="#hm-fea-box-item-icon-svg"></use>
-                    </svg> 
-                  </i>
-                </div>
-                <h4 class="hm-fea-box-item-title mHc2">Troef</h4>
-                <p>Quisque ante ex, facilisis in elit vel, pulvinar vestibulum ligula.</p>
-              </div>
-            </div>
-            <div class="hm-fea-box-item-col">
-              <div class="hm-fea-box-item mHc">
-                <a href="#" class="overlay-link"></a>
-                <div class="hm-fea-box-item-icon mHc1">
-                  <i>
-                    <svg class="hm-fea-box-item-icon-svg" width="38" height="38" viewBox="0 0 38 38" fill="#fff">
-                      <use xlink:href="#hm-fea-box-item-icon-svg"></use>
-                    </svg> 
-                  </i>
-                </div>
-                <h4 class="hm-fea-box-item-title mHc2">Troef</h4>
-                <p>Quisque ante ex, facilisis in elit vel, pulvinar vestibulum ligula.</p>
-              </div>
-            </div>
-            <div class="hm-fea-box-item-col">
-              <div class="hm-fea-box-item mHc">
-                <a href="#" class="overlay-link"></a>
-                <div class="hm-fea-box-item-icon mHc1">
-                  <i>
-                    <svg class="hm-fea-box-item-icon-svg" width="38" height="38" viewBox="0 0 38 38" fill="#fff">
-                      <use xlink:href="#hm-fea-box-item-icon-svg"></use>
-                    </svg> 
-                  </i>
-                </div>
-                <h4 class="hm-fea-box-item-title mHc2">Troef</h4>
-                <p>Quisque ante ex, facilisis in elit vel, pulvinar vestibulum ligula.</p>
-              </div>
-            </div>
+            <?php endforeach; ?>
           </div>
         </div>
       </div>
     </div>
   </div>
+<?php 
+endif;
+
+endif; 
+?>
 </section>
 
+<?php 
+  $showhide_pcats = get_field('showhide_pcats', HOMEID);
+  if( $showhide_pcats ):
+    $pcats = get_field('pcats', HOMEID);
+    if($pcats):
+?>
 <section class="hm-pro-bxes-section">
   <div class="hm-pro-bxes-sec-cntlr hmProBxesSlider clearfix">
+    <?php 
+    $pknop = '#';
+    foreach( $pcats as $pcat ): 
+      if( !empty($pcat['knop']) ):
+        $pknop = $pcat['knop'];
+      endif;
+    ?>
     <div class="hmProBxesSlide">
       <div class="hm-pro-bxe-item mHc">
         <div class="hm-pro-bxe-item-icon mHc1">
@@ -107,85 +113,68 @@
             </svg> 
           </i>
         </div>
-        <h3 class="hm-pro-bxe-item-title mHc2"><a href="#">Onbrandbare producten</a></h3>
-        <p>Vitae ridiculus dapibus morbi non, at orci leo volutpat integer. Aliquam ipsum sit magna est nulla nulla. Dictum feugiat consectetur in mauris, scelerisque mattis netus gravida.</p>
+        <?php  
+          if( !empty( $pcat['titel'] ) ) printf( '<h3 class="hm-pro-bxe-item-title mHc2"><a href="%s">%s</a></h3>', $pknop, $pcat['titel']); 
+          if( !empty( $pcat['beschrijving'] ) ) echo wpautop($pcat['beschrijving']); 
+        ?>
         <div class="hm-pro-bxe-item-more-link">
-          <a href="#">Ontdek onze oplossingen</a>
+          <a href="<?php echo $pknop; ?>">Ontdek onze oplossingen</a>
         </div>
       </div>
     </div>
-    <div class="hmProBxesSlide">
-      <div class="hm-pro-bxe-item mHc">
-        <div class="hm-pro-bxe-item-icon mHc1">
-          <i>
-            <svg class="hm-pro-bxe-item-icon-02-svg" width="84" height="84" viewBox="0 0 84 84" fill="#000062">
-              <use xlink:href="#hm-pro-bxe-item-icon-02-svg"></use>
-            </svg> 
-          </i>
-        </div>
-        <h3 class="hm-pro-bxe-item-title mHc2"><a href="#">Bouw -en industrie producten</a></h3>
-        <p>Vitae ridiculus dapibus morbi non, at orci leo volutpat integer. Aliquam ipsum sit magna est nulla nulla. Dictum feugiat consectetur in mauris, scelerisque mattis netus gravida.</p>
-        <div class="hm-pro-bxe-item-more-link">
-          <a href="#">Ontdek onze oplossingen</a>
-        </div>
-      </div>
-    </div>
-    <div class="hmProBxesSlide">
-      <div class="hm-pro-bxe-item mHc">
-        <div class="hm-pro-bxe-item-icon mHc1">
-          <i>
-            <svg class="hm-pro-bxe-item-icon-03-svg" width="84" height="84" viewBox="0 0 84 84" fill="#000062">
-              <use xlink:href="#hm-pro-bxe-item-icon-03-svg"></use>
-            </svg> 
-          </i>
-        </div>
-        <h3 class="hm-pro-bxe-item-title mHc2"><a href="#">Borduurwerken</a></h3>
-        <p>Vitae ridiculus dapibus morbi non, at orci leo volutpat integer. Aliquam ipsum sit magna est nulla nulla. Dictum feugiat consectetur in mauris, scelerisque mattis netus gravida.</p>
-        <div class="hm-pro-bxe-item-more-link">
-          <a href="#">Ontdek onze oplossingen</a>
-        </div>
-      </div>
-    </div>
-    
+    <?php endforeach; ?>
   </div>
 </section>
+<?php endif; ?>
+<?php endif; ?>
 
+<?php 
+  $showhide_overons = get_field('showhide_overons', HOMEID);
+  if( $showhide_overons ):
+    $hoverons = get_field('home_overons', HOMEID);
+    if($hoverons):
+    $ovafbeelding = '';
+    if( !empty($hoverons['afbeelding']) ){
+      $ovafbeelding = cbv_get_image_src($hoverons['afbeelding']);
+    }
+    $oblogos = $hoverons['logos'];
+?>
 <section class="hm-overons-section">
   <div class="container">
       <div class="row">
         <div class="col-md-12">
           <div class="clearfix hm-overons-sec-inr">
             <div class="hm-overons-sec-fea-img-cntlr">
-              <div class="hm-overons-sec-fea-img inline-bg" style="background: url(<?php echo THEME_URI; ?>/assets/images/hm-overons-sec-fea-img.jpg);"></div>
+              <div class="hm-overons-sec-fea-img inline-bg" style="background: url(<?php echo $ovafbeelding; ?>);"></div>
             </div>
             <div class="hm-overons-sec-des">
-              <h5 class="hm-overons-sec-des-sub-title">Over Ons</h5>
-              <h2 class="hm-overons-sec-des-title">Quisque ex at neque feugiat vehicula aliquet</h2>
+              <?php  
+                if( !empty( $hoverons['subtitel'] ) ) printf( '<h5 class="hm-overons-sec-des-sub-title">%s</h5>', $hoverons['subtitel']);  
+                if( !empty( $hoverons['titel'] ) ) printf( '<h2 class="hm-overons-sec-des-title">%s</h2>', $hoverons['titel']);  
+              ?>
+              <?php if( $oblogos ): ?>
               <div class="hm-overons-sec-des-logos clearfix">
+                <?php foreach($oblogos as $oblogo): ?>
                 <div class="hm-overons-des-logo">
                   <div class="hm-overons-des-logo-inr mHc">
-                    <img src="<?php echo THEME_URI; ?>/assets/images/hm-overons-sec-des-logo-01.png">
+                    <?php if( !empty($oblogo['logo']) ) echo cbv_get_image_tag($oblogo['logo']); ?>
                   </div>
                 </div>
-                <div class="hm-overons-des-logo">
-                  <div class="hm-overons-des-logo-inr mHc">
-                    <img src="<?php echo THEME_URI; ?>/assets/images/hm-overons-sec-des-logo-02.png">
-                  </div>
-                </div>
-                <div class="hm-overons-des-logo">
-                  <div class="hm-overons-des-logo-inr mHc">
-                    <img src="<?php echo THEME_URI; ?>/assets/images/hm-overons-sec-des-logo-02.png">
-                  </div>
-                </div>
+                <?php endforeach; ?>
               </div>
-              <p>ITP is gespecialiseerd in de fabricatie en handel van hittebestendige textieltoepassingen, alsook in het leveren van specifiek technische producten voor de Europese bouwsector en industrie.</p>
+              <?php endif; ?>
+              <?php if( !empty( $hoverons['beschrijving'] ) ) echo wpautop($hoverons['beschrijving']); ?>
               <div class="hm-overons-sec-des-btns">
-                <div class="hm-overons-sec-des-btn hm-overons-sec-des-btn-1">
-                  <a href="#">Lees meer</a>
-                </div>
-                <div class="hm-overons-sec-des-btn hm-overons-sec-des-btn-2">
-                  <a href="#">Onze aanpak</a>
-                </div>
+                <?php 
+                  $ovknop1 = $hoverons['knop_1'];
+                  $ovknop2 = $hoverons['knop_2'];
+                  if( is_array( $ovknop1 ) &&  !empty( $ovknop1['url'] ) ){
+                      printf('<div class="hm-overons-sec-des-btn hm-overons-sec-des-btn-1"><a href="%s" target="%s">%s</a></div>', $ovknop1['url'], $ovknop1['target'], $ovknop1['title']); 
+                  } 
+                  if( is_array( $ovknop2 ) &&  !empty( $ovknop2['url'] ) ){
+                      printf('<div class="hm-overons-sec-des-btn hm-overons-sec-des-btn-2"><a href="%s" target="%s">%s</a></div>', $ovknop2['url'], $ovknop2['target'], $ovknop2['title']); 
+                  } 
+                ?>
               </div>
             </div>
           </div>
@@ -193,31 +182,43 @@
       </div>
   </div>    
 </section>
+<?php endif; ?>
+<?php endif; ?>
 
+<?php 
+  $showhide_clients = get_field('showhide_clients', HOMEID);
+  if( $showhide_clients ):
+    $clientslogo = get_field('clientslogo', HOMEID);
+    if($clientslogo):
+?>
 <section class="hm-client-logos-sec">
   <div class="container">
     <div class="row">
       <div class="col-sm-12">
         <div class="hm-client-logos-sec-cntlr">
           <ul class="reset-list">
+            <?php foreach($clientslogo as $clogo): ?>
             <li>
               <div class="mHc1">
-                <img src="<?php echo THEME_URI; ?>/assets/images/client-logo-img-01.png">
+                <?php if( !empty($clogo['logo']) ) echo cbv_get_image_tag($clogo['logo']); ?>
               </div>
             </li>
-            <li>
-              <div class="mHc1">
-                <img src="<?php echo THEME_URI; ?>/assets/images/client-logo-img-02.png">
-              </div>
-            </li>
+            <?php endforeach; ?>
           </ul>
         </div>
       </div>
     </div>
   </div>
 </section>
+<?php endif; ?>
+<?php endif; ?>
 
-
+<?php 
+  $showhide_referenties = get_field('showhide_referenties', HOMEID);
+  if( $showhide_referenties ):
+    $hrefer = get_field('hreferenties', HOMEID);
+    if($hrefer):
+?>
 <section class="hm-our-references-sec">
   <div class="hm-our-references-sec-angle">
     <img src="<?php echo THEME_URI; ?>/assets/images/hm-our-references-sec-angle.png">
@@ -227,8 +228,10 @@
       <div class="row">
         <div class="col-sm-12">
           <div class="hm-our-references-sec-hdr">
-            <h3 class="horshdr-title">Onze referenties</h3>
-            <p>Vitae ridiculus dapibus morbi non, at orci leo volutpat integer. Aliquam ipsum sit magna est nulla nulla. Dictum feugiat consectetur in mauris, scelerisque mattis netus gravida.</p>
+            <?php  
+              if( !empty( $hrefer['titel'] ) ) printf( '<h3 class="horshdr-title">%s</h3>', $hrefer['titel']); 
+              if( !empty( $hrefer['beschrijving'] ) ) echo wpautop($hrefer['beschrijving']); 
+            ?>
           </div>
         </div>
         <div class="col-sm-12">
@@ -472,7 +475,8 @@
     </div>
   </div>
 </section>
-
+<?php endif; ?>
+<?php endif; ?>
 
 
 
